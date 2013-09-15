c2583a20c214b227f075085c8368aefc2ef5efc3
Neil Parley <mail@neilparley.co.uk>
2013-09-15 18:31:17 +0100

    Add user checking to the lastlocation endpoint

--- backend.py
+++ backend.py
@@ -4,12 +4,42 @@ from protorpc import remote
 
 import os
 import sys
+import auth_util
 
 ENDPOINTS_PROJECT_DIR = os.path.join(os.path.dirname(__file__),
                                      'endpoints-proto-datastore')
 sys.path.append(ENDPOINTS_PROJECT_DIR)
 
 from endpoints_proto_datastore.ndb import EndpointsModel
+
+def checkUser(userID):
+  if userID:
+    userCheck = Users.get_by_id(userID)
+    if userCheck:
+      return True
+    else:
+      return False
+  else:
+    return False
+ 
+def checkOwnerUser(userID):
+  if userID:
+    userCheck = Users.get_by_id(userID)
+    if userCheck:
+      if userCheck.owner == True:
+        return True
+      else:
+        return False
+    else:
+      return False
+  else:
+    return False 
+
+class Users(ndb.Model):
+    userid = ndb.StringProperty()
+    owner = ndb.BooleanProperty()
+    name = ndb.StringProperty()
+    picture = ndb.StringProperty()
 
 class Location(EndpointsModel):
     timestampMs = ndb.IntegerProperty()
@@ -21,13 +51,17 @@ class Location(EndpointsModel):
     altitude = ndb.IntegerProperty()
     verticalAccuracy = ndb.IntegerProperty()
 
-@endpoints.api(name='mylatapi', version='v1', description='Rest API to the location data')
+@endpoints.api(name='mylatitude', version='v1', description='Rest API to the location data')
 class myLatAPI(remote.Service):
   @Location.query_method(user_required=True,
                         path='lastLocation', name='location.last',limit_default=1,limit_max=1)
   def lastLocation(self, query):
-    #need to check user
-    return query.order(-Location.timestampMs)
+    userID = auth_util.get_google_plus_user_id()
+    if checkUser(userID):
+      return query.order(-Location.timestampMs)
+    else:
+      message = 'User does not have access to this endpoint'
+      raise endpoints.UnauthorizedException(message)
   
 application = endpoints.api_server([myLatAPI], restricted=False)
 
