d467f06c7e60773916ff95bf6986ec84646c612b
Neil Parley <mail@neilparley.co.uk>
2013-09-29 18:46:30 +0100

    Moved some of the javacript out of the html file

---
+++ js/map.js
@@ -0,0 +1,106 @@
+/**
+ * Created by Neil on 29/09/13.
+ */
+
+function signin(mode, callback,clientID) { // clientID filled in by template, immediate = true because we should not need to ask permission again
+    gapi.auth.authorize({client_id: clientID,
+            scope: ["https://www.googleapis.com/auth/userinfo.email", "https://www.googleapis.com/auth/userinfo.profile"], immediate: true,
+            response_type: 'token'}, // Can't use id tokens as we can't get the user if from an id token
+        callback);
+}
+
+function userAuthed() {
+
+    var request =
+        gapi.client.oauth2.userinfo.get().execute(function (resp) { // Check the token by calling userinfo, if it's ok call our end point
+            if (!resp.code) {
+                var token = gapi.auth.getToken();
+                gapi.client.mylatitude.location.last().execute(function (resp) { // this does not do anything yet it's just a test.
+//                    console.log(resp);
+                });
+            }
+        });
+}
+
+function initialize() {
+    var mapOptions = {
+        zoom: 16,
+        center: new google.maps.LatLng(locations[0].latitude, locations[0].longitude),
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+    var map = new google.maps.Map(document.getElementById('map_canvas'),
+        mapOptions);
+
+    var locationCircle = null;
+    var locationAccuracy = null;
+    var infoWindow = new google.maps.InfoWindow({
+        content: "test"
+    });
+
+    function updateLocation(location,userName,userPic) {
+        if (location.timeStamp > 0) {
+            if (locationCircle != null) locationCircle = null;
+            if (locationAccuracy != null) locationAccuracy = null;
+            var center = new google.maps.LatLng(location.latitude, location.longitude);
+            infoWindow.close();
+            infoWindow.setPosition(center);
+            locationAccuracy = new google.maps.Circle({
+                center: center,
+                radius: location.accuracy,
+                strokeColor: "#66CCFF",
+                strokeOpacity: 0.8,
+                strokeWeight: 2,
+                fillColor: "#66CCFF",
+                fillOpacity: 0.35,
+                map: map
+            });
+            locationCircle = new google.maps.Circle({
+                center: center,
+                radius: 5,
+                strokeColor: "#6699FF",
+                strokeOpacity: 0.8,
+                strokeWeight: 2,
+                fillColor: "#6699FF",
+                fillOpacity: 0.8,
+                map: map
+            });
+            var d = new Date(location.timeStamp)
+            var dNow = new Date()
+            var diff = dNow.getTime() - d.getTime();
+            diff /= 60000;
+            diffText = "minutes";
+            if (diff > 60) {
+                diff /= 60;
+                diffText = "hours"
+            }
+            diff = Math.round(diff);
+            infoWindow.setPosition(center);
+            updateString = "<img src='"+userPic+"' height='50' width='50' style='float:right;border-width:1px;border-style:solid;border-color:#A5A5A5;'> <strong>"+userName+"'s location </strong><br/>";
+            updateString += "<a href='http://maps.google.com/maps?saddr=&daddr=" + location.latitude + "," + location.longitude + "'> Directions Link</a><br/>";
+            updateString += "Last updated on:<br/>" + d.toDateString() + "<br/>";
+            updateString += "at: " + d.toTimeString() + "<br/>";
+            updateString += diff + " " + diffText + " ago <br/><span style=\"font-size:10%\">&nbsp;</span> ";
+
+            var div = document.createElement('div');
+            div.className = "infoWin";
+            div.innerHTML = updateString;
+
+            infoWindow.setContent(div);
+
+            infoWindow.open(map);
+            google.maps.event.addListener(locationAccuracy, 'click', function () {
+                infoWindow.open(map);
+            });
+            google.maps.event.addListener(locationCircle, 'click', function () {
+                infoWindow.open(map);
+            });
+
+
+        }
+    }
+
+    updateLocation(locations[0],userName,userPic);
+
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
