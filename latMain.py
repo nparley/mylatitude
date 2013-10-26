30019517dcde3b921d5508561441e20d4e305f07
Neil Parley <mail@neilparley.co.uk>
2013-10-26 23:59:36 +0100

    Update database classes to show what values are required

--- latMain.py
+++ latMain.py
@@ -189,10 +189,10 @@ class Location(ndb.Model):
     """
     Database Location Class: for storing location data
     """
-    timestampMs = ndb.IntegerProperty()
-    latitudeE7 = ndb.IntegerProperty()
-    longitudeE7 = ndb.IntegerProperty()
-    accuracy = ndb.IntegerProperty()
+    timestampMs = ndb.IntegerProperty(required=True)
+    latitudeE7 = ndb.IntegerProperty(required=True)
+    longitudeE7 = ndb.IntegerProperty(required=True)
+    accuracy = ndb.IntegerProperty(required=True)
     velocity = ndb.IntegerProperty()
     heading = ndb.IntegerProperty()
     altitude = ndb.IntegerProperty()
@@ -203,7 +203,7 @@ class Maps(ndb.Model):
     """
     Database Maps Class: for storing Google Maps API key
     """
-    keyid = ndb.StringProperty()
+    keyid = ndb.StringProperty(required=True)
 
 
 class Users(ndb.Model):
@@ -224,32 +224,33 @@ class TimeZones(ndb.Model):
     """
     Database TimeZone Class: for storing the Timezone for a day
     """
-    day = ndb.DateProperty()
-    dstOffset = ndb.IntegerProperty()
-    rawOffset = ndb.IntegerProperty()
-    timeZoneId = ndb.StringProperty()
-    timeZoneName = ndb.StringProperty()
+    day = ndb.DateProperty(required=True)
+    dstOffset = ndb.IntegerProperty(required=True)
+    rawOffset = ndb.IntegerProperty(required=True)
+    timeZoneId = ndb.StringProperty(required=True)
+    timeZoneName = ndb.StringProperty(required=True)
 
 
 class Keys(ndb.Model):
     """
     Database Keys Class: holds the backitude access key
     """
-    keyid = ndb.StringProperty()
+    keyid = ndb.StringProperty(required=True)
 
 
 class FriendUrls(ndb.Model):
     """
     Database Friends URL Class: holds the random keys to allow friends access
     """
-    keyid = ndb.StringProperty()
+    keyid = ndb.StringProperty(required=True)
+    expires = ndb.IntegerProperty(default=0)
 
 
 class SetupFormKey(ndb.Model):
     """
     Holds the setup form key
     """
-    keyid = ndb.StringProperty()
+    keyid = ndb.StringProperty(required=True)
 
 #class oauthTest(webapp2.RequestHandler):
 #  @decorator.oauth_required
